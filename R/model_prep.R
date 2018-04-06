40151471f1a4b71b004d7737a6ced0beaf5bf6cd
edgararuiz <edgararuiz@gmail.com>
2018-04-06 18:50:33 -0500

    Documents add_dummy_variables()

--- R/model_prep.R
+++ R/model_prep.R
@@ -1,3 +1,28 @@
+#' Creates dummy variables
+#'
+#' It uses 'tidyeval' and 'dplyr' to create dummy variables based for
+#' categorical variables. 
+#'
+#' @param df A Local or remote data frame
+#' @param x Categorical variable
+#' @param values Possible known values of the categorical variable. If not passed
+#' then the function will take an additional step to figure the unique values of
+#' the variable.
+#' @param auto_values Safeguard argument to prevent the function from figuring the
+#' unique values if the values argument is empty.  If it is ok for this function
+#' to obtain the unique values, set to TRUE.  Defaults to FALSE.
+#' @param remove_original It removes the original variable from the returned table.
+#' Defaults to TRUE.
+#'
+#' @examples
+#' library(dplyr)
+#' 
+#' mtcars %>%
+#'   add_dummy_variables(cyl, values = c(4, 6, 8))
+#' 
+#' mtcars %>%
+#'   add_dummy_variables(cyl, auto_values = TRUE)
+#'
 #' @export
 add_dummy_variables <- function(df, x, values = c(), 
                                 auto_values = FALSE, remove_original = TRUE){
@@ -7,11 +32,16 @@ add_dummy_variables <- function(df, x, values = c(),
   var_found <- expr_text(x) %in% colnames(df)
   if(!var_found) stop("Variable not found")
   
-  if(length(values) == 0 & auto_values == TRUE){
-    values <- df %>% 
-      group_by(!! x) %>% 
-      summarise() %>% 
-      pull()
+  if(length(values) == 0){
+    if(auto_values == TRUE){
+      values <- df %>% 
+        group_by(!! x) %>% 
+        summarise() %>% 
+        pull()
+    } else {
+      stop("No values provided and auto_values is set to FALSE")
+    }
+
   }
   
  vals <- values %>%
--- R/tidypredict.R
+++ R/tidypredict.R
@@ -1,3 +1,17 @@
+#' @export
+as_parsed_model <- function(df, model_type = "lm") {
+  df <- tidyr::gather(df, var, val)
+  td <- start_parse(model_type = model_type)
+  for (i in seq_len(nrow(df))) {
+    td <- td %>%
+      add_term(
+        df$var[i],
+        df$val[i]
+      )
+  }
+  td
+}
+
 start_parse <- function(model_type = "lm") {
   bind_rows(
     tibble(
@@ -38,16 +52,4 @@ add_term <- function(df, names, estimate) {
 
 get_marker <- function() "{{:}}"
 
-#' @export
-tidypredict_parser <- function(df, model_type = "lm") {
-  df <- tidyr::gather(df, var, val)
-  td <- start_parse(model_type = model_type)
-  for (i in seq_len(nrow(df))) {
-    td <- td %>%
-      add_term(
-        df$var[i],
-        df$val[i]
-      )
-  }
-  td
-}
+
