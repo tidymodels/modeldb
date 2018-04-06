d4f33dccfffcba24ef55f5b6a961f48dc3eb73bf
edgararuiz <edgararuiz@gmail.com>
2018-04-06 15:40:14 -0500

    Completes model parser

--- R/tidypredict.R
+++ R/tidypredict.R
@@ -1,53 +1,46 @@
-start_parse <- function(){
-  tibble(
-    labels = "labels",
-    estimate = 0,
-    type = "variable"
-  )
+start_parse <- function(model_type = "lm"){
+  bind_rows(
+    tibble(
+      labels = "labels",
+      estimate = 0,
+      type = "variable",
+      vals = NA),
+    tibble(
+      labels = "model",
+      estimate = NA,
+      type = "variable",
+      vals = model_type))
 }
 
 add_term <- function(df, names, estimate){
-  
   if(names == "Intercept") names <- "(Intercept)"
-  
   df <- df %>%
     bind_rows(
       tibble(
         labels = !! names,
         estimate = !! estimate,
-        type = "variable"
-      )
-    )
+        type = "term",
+        vals = NA))
   
   if(names != "(Intercept)") {
-    curr_field <- ncol(df)  - 2
-    field <- c(rep(NA, nrow(df) - 1), get_marker())
-    
+    curr_field <- ncol(df)  - 3
+    field <- c(names ,rep(NA, nrow(df) - 2), get_marker())
     new_field <- sym(paste0("field_", curr_field))
-    df <- df %>%
-      mutate(!! new_field := field)
-    
-    df <- df %>%
-      mutate(!! new_field := ifelse(labels == "labels", names, NA))
+    df <- mutate(df, !! new_field := field)
   }
-  
   df
 }
 
-get_marker <- function() "{:}"
-
-get_marker_regx <- function() "\\{\\:\\}"
+get_marker <- function() "{{:}}"
 
 #' @export
-tidypredict_parser <- function(df){
-  td <- start_parse()
+tidypredict_parser <- function(df, model_type = "lm"){
+  td <- start_parse(model_type = model_type)
   for(i in seq_len(nrow(df))) {
     td <- td %>%
       add_term(
         df$var[i],
-        df$val[i]
-      )
-  }
-  
+        df$val[i])
+    }
   td
 }
