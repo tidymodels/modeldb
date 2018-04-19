ab8c448ea46635e08a1c878ff5d2c2e3cb17f60d
edgararuiz <edgararuiz@gmail.com>
2018-04-19 09:22:08 -0500

    Adds testthat and initial tests

---
+++ tests/testthat/test_dummy_var.R
@@ -0,0 +1,20 @@
+context("add_dummy_variables")
+
+test_that("Function create the correct columns", {
+ cols_expected <- c("mpg","disp","hp","drat","wt","qsec","vs","am","gear","carb","cyl_6","cyl_8")
+ expect_equal(
+    colnames(add_dummy_variables(mtcars, cyl, c(4,6,8))),
+    cols_expected
+  )
+  expect_equal(
+    colnames(add_dummy_variables(mtcars, cyl, auto_values = TRUE)),
+    cols_expected
+  )
+})
+
+test_that("Function fails when no values are passed and auto_values is FALSE",{
+  expect_error(add_dummy_variables(mtcars, cyl))
+  expect_error(add_dummy_variables(mtcars, cyl, auto_values = FALSE))
+})
+
+
